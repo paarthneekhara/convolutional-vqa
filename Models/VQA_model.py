bd5d45b1cc5b5e9b8407610051851e73d2a6b354
Paarth <paarth.n@gmail.com>
2017-07-11 11:26:44 +0530

    Setting up models/code

---
+++ Models/VQA_model.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+import numpy as np
+import text_model
+
+class VQA_model:
+    def __init__(self, options):
+
+
+        # TODO
+        self.options = options
+        self.w_image_embedding = tf.get_variable('w_image_embedding', 
+            [options['fc7_feature_length'], options['residual_channels']],
+            initializer=tf.truncated_normal_initializer(stddev=0.02))
+
+        self.b_image_embedding = tf.Variable(tf.zeros([options['residual_channels']]), name='b_image_embedding')
+        
+        self.w_ans =  tf.get_variable('w_ans', 
+            [options['residual_channels'], options['ans_vocab_size']],
+            initializer=tf.truncated_normal_initializer(stddev=0.02))
+        self.b_ans = tf.Variable(tf.zeros([options['ans_vocab_size']]), name='b_ans')
+
+
+    def build_model(self):
+        options = self.options
+        
+        fc7_features = tf.placeholder('float32',[ None, self.options['fc7_feature_length'] ], name = 'fc7')
+        answer = tf.placeholder('float32', [None, self.options['ans_vocab_size']], name = "answer")
+        
+        tm = text_model.TextModel(options)
+        text_tensors = tm.build_model()
+        source_sentence = text_tensors['source_sentence']
+
+        encoded_sentence = text_tensors['encoded_sentence']
+        encoded_shape = encoded_sentence.get_shape()
+        sentence_embedding = tf.slice(encoded_sentence, [0, options['text_length'] - 1, 0], 
+            [-1, -1, -1])
+
+        image_embedding = tf.matmul(fc7_features, self.w_image_embedding) + self.b_image_embedding
+        image_embedding = tf.nn.tanh(image_embedding)
+
+        combined_embedding = sentence_embedding + image_embedding
+        logits = tf.matmul(lstm_answer, self.w_ans) + self.b_ans
+
+        ce = tf.nn.softmax_cross_entropy_with_logits(logits, answer, name = 'ce')
+        answer_probab = tf.nn.softmax(logits, name='answer_probab')
+        predictions = tf.argmax(answer_probab,1)
+        correct_predictions = tf.equal(tf.argmax(answer_probab,1), tf.argmax(answer,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+
+        loss = tf.reduce_sum(ce, name = 'loss')
+
+        input_tensors = {
+            'fc7' : fc7_features,
+            'source_sentence' : source_sentence,
+            'answer' : answer
+        }
+
+        return input_tensors, loss, accuracy, predictions
+
+
+
+        
+
+
+
+        
+
+def main():
+    options = {
+        'n_source_quant' : 1000,
+        'residual_channels' : 512,
+        'batch_size' : 64,
+        'text_length' : 25,
+        'encoder_filter_width' : 3,
+        'encoder_dilations' : [1, 2, 4, 8, 16,
+                          1, 2, 4, 8, 16],
+        'fc7_feature_length' : 4096
+
+    }
+
+    vqa = VQA_model(options)
+    vqa.build_model()
+
+if __name__ == '__main__':
+    main()
---
+++ Models/text_model.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+import numpy as np
+import ops
+
+class TextModel:
+    def __init__(self, options):
+        '''
+        options
+        n_source_quant : quantization channels of source text
+        residual_channels : number of channels in internal blocks
+        batch_size : Batch Size
+        text_length : Number of words in sentece
+        encoder_filter_width : Encoder Filter Width
+        encoder_dilations : Dilation Factor for decoder layers (list)
+        
+        '''
+        self.options = options
+
+        self.w_source_embedding = tf.get_variable('w_source_embedding', 
+            [options['n_source_quant'], 2*options['residual_channels']],
+            initializer=tf.truncated_normal_initializer(stddev=0.02))
+
+    def build_model(self):
+        options = self.options
+        source_sentence = tf.placeholder('int32', [options['batch_size'], options['text_length']], name = 'sentence')
+        souce_embedding = tf.nn.embedding_lookup(self.w_source_embedding, source_sentence)
+
+        encoded_sentence = self.encoder(souce_embedding)
+
+        return {
+            'source_sentence' : source_sentence,
+            'encoded_sentence' : encoded_sentence
+        }
+
+
+    def encode_layer(self, input_, dilation, layer_no, last_layer = False):
+        options = self.options
+        relu1 = tf.nn.relu(input_, name = 'enc_relu1_layer{}'.format(layer_no))
+        conv1 = ops.conv1d(relu1, options['residual_channels'], name = 'enc_conv1d_1_layer{}'.format(layer_no))
+        
+        relu2 = tf.nn.relu(conv1, name = 'enc_relu2_layer{}'.format(layer_no))
+        dilated_conv = ops.dilated_conv1d(relu2, options['residual_channels'], 
+            dilation, options['encoder_filter_width'],
+            causal = False, 
+            name = "enc_dilated_conv_layer{}".format(layer_no)
+            )
+        
+        relu3 = tf.nn.relu(dilated_conv, name = 'enc_relu3_layer{}'.format(layer_no))
+        conv2 = ops.conv1d(relu3, 2 * options['residual_channels'], name = 'enc_conv1d_2_layer{}'.format(layer_no))
+        return input_ + conv2
+
+    def encoder(self, input_):
+        options = self.options
+        curr_input = input_
+        for layer_no, dilation in enumerate(self.options['encoder_dilations']):
+            layer_output = self.encode_layer(curr_input, dilation, layer_no)
+            # ENCODE ONLY TILL THE INPUT LENGTH, conditioning should be 0 beyond that
+            # layer_output = tf.mul(layer_output, self.souce_embedding, name = 'layer_{}_output'.format(layer_no))
+            curr_input = layer_output
+        
+        # TO BE CONCATENATED WITH TARGET EMBEDDING
+        processed_output = tf.nn.relu( ops.conv1d(tf.nn.relu(layer_output), 
+            options['residual_channels'], 
+            name = 'encoder_post_processing') )
+        return processed_output
+
+
+def main():
+    options = {
+        'n_source_quant' : 1000,
+        'residual_channels' : 512,
+        'batch_size' : 64,
+        'text_length' : 25,
+        'encoder_filter_width' : 3,
+        'encoder_dilations' : [1, 2, 4, 8, 16,
+                          1, 2, 4, 8, 16]
+    }
+
+    tm = TextModel(options)
+    tm.build_model()
+
+if __name__ == '__main__':
+    main()
